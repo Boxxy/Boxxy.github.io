36d63bcf2c70f26917b5c516b127e1e8c8d7d874
Clayton Hawken <claytonhawken@gmail.com>
2018-06-13 17:26:50 -0500

    Skinning updates

--- html/html.nocache.js
+++ html/html.nocache.js
@@ -375,11 +375,11 @@ function html(){
     }
     var strongName;
     try {
-      unflattenKeylistIntoAnswers(['gecko1_8'], 'B6E10AB89FB039CE19E84E33D15E01E4');
-      unflattenKeylistIntoAnswers(['ie10'], 'B6E10AB89FB039CE19E84E33D15E01E4' + ':1');
-      unflattenKeylistIntoAnswers(['ie8'], 'B6E10AB89FB039CE19E84E33D15E01E4' + ':2');
-      unflattenKeylistIntoAnswers(['ie9'], 'B6E10AB89FB039CE19E84E33D15E01E4' + ':3');
-      unflattenKeylistIntoAnswers(['safari'], 'B6E10AB89FB039CE19E84E33D15E01E4' + ':4');
+      unflattenKeylistIntoAnswers(['gecko1_8'], 'F68AE07188522BB7E75C799FD6988B26');
+      unflattenKeylistIntoAnswers(['ie10'], 'F68AE07188522BB7E75C799FD6988B26' + ':1');
+      unflattenKeylistIntoAnswers(['ie8'], 'F68AE07188522BB7E75C799FD6988B26' + ':2');
+      unflattenKeylistIntoAnswers(['ie9'], 'F68AE07188522BB7E75C799FD6988B26' + ':3');
+      unflattenKeylistIntoAnswers(['safari'], 'F68AE07188522BB7E75C799FD6988B26' + ':4');
       strongName = answers[computePropValue('user.agent')];
       var idx = strongName.indexOf(':');
       if (idx != -1) {
