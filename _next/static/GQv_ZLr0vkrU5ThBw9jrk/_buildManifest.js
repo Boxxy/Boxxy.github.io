7235ce961f9ad2ae4b02d95e5483d80a2063c156
Boxxy <claytonhawken@gmail.com>
2022-03-18 16:17:15 -0500

    add a few more songs, show guesses

---
+++ _next/static/GQv_ZLr0vkrU5ThBw9jrk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/26-663221dfaebfda8b.js","static/chunks/pages/index-e61b25c1f0bfa7a0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
