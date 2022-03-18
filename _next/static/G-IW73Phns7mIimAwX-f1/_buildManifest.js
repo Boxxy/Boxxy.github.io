309eb2c2c859dbe6d53f8aa04182f7d5fbfaa09e
Boxxy <claytonhawken@gmail.com>
2022-03-18 15:35:49 -0500

    update

---
+++ _next/static/G-IW73Phns7mIimAwX-f1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/880-d101de8c2a52d51a.js","static/chunks/pages/index-3bdca6925736fb6c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/mgUMGpOENEceAhfjJ1qEO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/880-d101de8c2a52d51a.js","static/chunks/pages/index-37ca9119150f303c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
