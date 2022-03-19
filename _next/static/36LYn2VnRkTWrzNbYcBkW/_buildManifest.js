a57440555a727031901aa4f5c93f971112d3ae1d
Boxxy <claytonhawken@gmail.com>
2022-03-18 20:48:31 -0500

    I hate github sometimes

---
+++ _next/static/36LYn2VnRkTWrzNbYcBkW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/26-663221dfaebfda8b.js","static/chunks/pages/index-1fd430a02e9f9108.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
